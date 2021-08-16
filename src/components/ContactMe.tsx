e058df578d620ef28bf818b71b601afd792eb8b4
Jens Schlegel <jens.b.schlegel@gmail.com>
2021-08-16 12:06:09 +0200

    font size contact field

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -4,7 +4,7 @@ export default function ContactMe() {
 	return (
 		<div className="w-screen h-screen p-6 md:p-12 lg:p-24" id="contactMe">
 			<h1>Kontaktiere mich</h1>
-			<div className="flex flex-col items-center h-1/2 justify-evenly text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+			<div className="flex flex-col items-center h-1/2 justify-evenly text-md sm:text-2xl md:text-3xl lg:text-4xl">
 				<CopyToClipboard name="jens.schlegel@protonmail.com" />
 			</div>
 		</div>
