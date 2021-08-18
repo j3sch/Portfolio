0519e811a211b70af19791e2b42f3a652744b6d6
Jens Schlegel <jens.b.schlegel@gmail.com>
2021-08-18 20:55:07 +0200

    size contact field smartphone

--- src/ui/CopyClipboardBtn.tsx
+++ src/ui/CopyClipboardBtn.tsx
@@ -14,7 +14,7 @@ export default function CopyClipboardBtn({
 	return (
 		<div className="p-2 pl-1">
 			<button
-				className="bg-white ring-1 hover:ring-2 ring-gray-300 rounded-lg w-[8.75rem] sm:w-[13.75rem] md:w-[17.25rem] lg:w-[20rem] xl:w-[33rem] h-16 md:h-24 flex items-center text-center justify-center text-lg sm:text-2xl"
+				className="bg-white ring-1 hover:ring-2 ring-gray-300 rounded-lg w-[8.875rem] sm:w-[13.75rem] md:w-[17.25rem] lg:w-[20rem] xl:w-[33rem] h-16 md:h-24 flex items-center text-center justify-center text-lg sm:text-2xl"
 				onClick={() => {
 					navigator.clipboard.writeText(copyText);
 					setClicked(true);
