c65d4b032668ef6765a5b3623cccbcf611463b75
Jens Schlegel <jens.b.schlegel@gmail.com>
2021-08-18 15:20:40 +0200

    swipe in Skills implemented

---
+++ src/lib/getNumberOfCards.ts
@@ -0,0 +1,17 @@
+import { useWindowWidth } from '@react-hook/window-size';
+
+let width = 0;
+
+export default function getNumberOfCards() {
+	width = useWindowWidth();
+
+	if (width <= 800) {
+		return 1;
+	} else if (width <= 1350) {
+		return 2;
+	} else if (width <= 1636) {
+		return 3;
+	} else {
+		return 4;
+	}
+}
